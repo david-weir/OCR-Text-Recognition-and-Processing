a1a00e6dfbfb3cfda801e9cc9adc7fdcc9b623cc
David Weir <david.weir4@mail.dcu.ie>
2023-02-01 23:56:03 +0000

    Script to handle a folder of images containing printed text

---
+++ src/folder_img_ocr.py
@@ -0,0 +1,33 @@
+# extracts text from all images in a folder and outputs a text file of the extracted text
+from PIL import Image
+import pytesseract
+import os
+
+
+def main():
+    # pytesseract path
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    # path to the folder of images
+    folderpath = "../test/printOCR/eng/Romeo and Juliet, William Shakespeare"
+
+    # link to the file in which output needs to be stored
+    resultpath = "./r+j.txt"
+
+    # iterate through and OCR images in folder
+    for image in os.listdir(folderpath):
+        img = Image.open(os.path.join(folderpath, image))  # access the image
+
+        # convert image to text (PLACEHOLDER eng for tests)
+        text = pytesseract.image_to_string(img, lang="eng")
+
+        # append text to the new text file (creating a file if it does not already exist)
+        file1 = open(resultpath, "a+")
+
+        # write to text file + close
+        file1.write(text + "/n")
+        file1.close()
+
+
+if __name__ == '__main__':
+    main()
