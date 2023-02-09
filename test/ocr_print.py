60b80a2442656c2df5a34a62b44048a6f71dad2c
David Weir <david.weir4@mail.dcu.ie>
2023-02-09 02:09:39 +0000

    Pathing issues fix

--- test/ocr_print.py
+++ test/ocr_print.py
@@ -17,10 +17,10 @@ class TestPrintOCR(unittest.TestCase):
 
     # edit distance test for Grimm's Fairy Tales
     def grimms(self):
-        with open("./print text testing/ocr/Grimm's Fairy Tales.txt", 'r', encoding="latin-1") as f1:
+        with open(".test/print text testing/ocr/Grimm's Fairy Tales.txt", 'r', encoding="latin-1") as f1:
             ocr = f1.read().strip().replace('/n', '')
 
-        with open("./print text testing/gr/Grimms' Fairy Tales by Jacob Grimm and Wilhelm Grimm.txt", 'r',
+        with open(".test/print text testing/gr/Grimms' Fairy Tales by Jacob Grimm and Wilhelm Grimm.txt", 'r',
                   encoding="latin-1") as f2:
             ground = f2.read().strip().replace('/n', '')
 
