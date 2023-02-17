526876e1febd83e62a2d1f0d4129dcafc0648a39
David Weir <david.weir4@mail.dcu.ie>
2023-02-17 02:14:45 +0000

    Extended text matching tests to include additional data folders

--- test/ocr_print.py
+++ test/ocr_print.py
@@ -32,5 +32,70 @@ class TestPrintOCR(unittest.TestCase):
         self.assertGreaterEqual(edit_dist, 0.9, "Grimm's Fairy Tales Test FAILED - edit distance below 0.9")
 
 
+    def test_drac(self):
+        with open("./test/print text testing/ocr/Dracula.txt", 'r', encoding="latin-1") as f1:
+            ocr = f1.read().strip().replace('/n', '')
+
+        with open("./test/print text testing/ground texts/Dracula by Bram Stoker.txt", 'r',
+                  encoding="latin-1") as f2:
+            ground = f2.read().strip().replace('/n', '')
+
+        edit_dist = self.normalise_levenshtein(ocr, ground)
+
+        f1.close()
+        f2.close()
+
+        self.assertGreaterEqual(edit_dist, 0.9, "Dracula FAILED - edit distance below 0.9")
+
+
+    def test_gatsby(self):
+        with open("./test/print text testing/ocr/Great Gatsby.txt", 'r', encoding="latin-1") as f1:
+            ocr = f1.read().strip().replace('/n', '')
+
+        with open("./test/print text testing/ground texts/The Great Gatsby.txt", 'r',
+                  encoding="latin-1") as f2:
+            ground = f2.read().strip().replace('/n', '')
+
+        edit_dist = self.normalise_levenshtein(ocr, ground)
+
+        f1.close()
+        f2.close()
+
+        self.assertGreaterEqual(edit_dist, 0.9, "The Great Gatsby FAILED - edit distance below 0.9")
+
+
+    def test_randj(self):
+        with open("./test/print text testing/ocr/Romeo and Juliet, William Shakespeare.txt", 'r',
+                  encoding="latin-1") as f1:
+            ocr = f1.read().strip().replace('/n', '')
+
+        with open("./test/print text testing/ground texts/"
+                  "The Project Gutenberg of Romeo and Juliet, William Shakespeare.txt", 'r', encoding="latin-1") as f2:
+            ground = f2.read().strip().replace('/n', '')
+
+        edit_dist = self.normalise_levenshtein(ocr, ground)
+
+        f1.close()
+        f2.close()
+
+        self.assertGreaterEqual(edit_dist, 0.9, "Romeo & Juliet FAILED - edit distance below 0.9")
+
+
+    def test_oss(self):
+        with open("./test/print text testing/ocr/oss handbook.txt", 'r', encoding="latin-1") as f1:
+            ocr = f1.read().strip().replace('/n', '')
+
+        with open("./test/print text testing/ground texts/Simple Sabotage Field Manual by United States.txt", 'r',
+                  encoding="latin-1") as f2:
+            ground = f2.read().strip().replace('/n', '')
+
+        edit_dist = self.normalise_levenshtein(ocr, ground)
+
+        f1.close()
+        f2.close()
+
+        self.assertGreaterEqual(edit_dist, 0.9, "Simple Sabotage Field Manual OSS FAILED - edit distance below 0.9")
+
+
 if __name__ == '__main__':
     unittest.main()
