3521c94bb5d6dc4ed562eaaa21b9d1cf3d88e471
David Weir <david.weir4@mail.dcu.ie>
2023-02-17 15:49:41 +0000

    Updated print ocr script with folders to temporarily run on testing dataset until integration with front end data upload functionality

--- src/folder_img_ocr.py
+++ src/folder_img_ocr.py
@@ -5,28 +5,28 @@ import os
 
 
 def main():
-    # pytesseract path
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
     # path to the folder of images
-    folderpath = "../test/printOCR/eng/Romeo and Juliet, William Shakespeare"
+    folderpath = "./test/print text testing/data/"
 
     # link to the file in which output needs to be stored
-    resultpath = "./r+j.txt"
+    for folder in os.listdir(folderpath):
+        # link to the file in which output needs to be stored
+        resultpath = "./src/ocr_results" + (folder + ".txt")
 
-    # iterate through and OCR images in folder
-    for image in os.listdir(folderpath):
-        img = Image.open(os.path.join(folderpath, image))  # access the image
+        # iterate through and OCR images in folder
+        for image in os.listdir(os.path.join(folderpath, folder)):
+            img = Image.open(os.path.join(os.path.join(folderpath, folder), image))  # access the image
 
-        # convert image to text (PLACEHOLDER eng for tests)
-        text = pytesseract.image_to_string(img, lang="eng")
+            # convert image to text (PLACEHOLDER eng for tests)
+            text = pytesseract.image_to_string(img, lang="eng")
 
-        # append text to the new text file (creating a file if it does not already exist)
-        file1 = open(resultpath, "a+")
+            # append text to the new text file (creating a file if it does not already exist)
+            file1 = open(resultpath, "a+")
 
-        # write to text file + close
-        file1.write(text + "/n")
-        file1.close()
+            # write to text file + close
+            file1.write(text + "/n")
+            file1.close()
 
 
 if __name__ == '__main__':
