76400cb21a6acf2a607ab50f0f5bc0f99f395935
Róisín O'Rourke <roisin.orourke28@mail.dcu.ie>
2023-03-31 20:20:20 +0100

    improved layout of final pdf

--- src/convert.py
+++ src/convert.py
@@ -27,46 +27,26 @@ def play_mp3(lang, file):
     # os.system("mpg321 speech.mp3")
 
 
-"""def covert_to_pdf(file):
-    pdf = FPDF()  
-    
-    # Add a page
-    pdf.add_page()
-    
-    # set style and size of font
-    # that you want in the pdf
-    pdf.set_font("Arial", size = 11)
-    
-    # open the text file in read mode
-    f = open(file, "r")
-    
-    # insert the texts in pdf
-    for x in f:
-        pdf.cell(w=200, h=10, txt = x, ln = 1, align = 'L')
-    
-    # save the pdf with name .pdf
-    pdf.output("mygfg.pdf") """
-
 # convert_to_mp3("en", "text.txt")
-# covert_to_pdf("text.txt")
 # https://stackoverflow.com/questions/10112244/convert-plain-text-to-pdf-in-python
 def convert_to_pdf(text, filename):
     a4_width_mm = 210
-    pt_to_mm = 0.35
-    fontsize_pt = 10
+    pt_to_mm = 0.5
+    fontsize_pt = 12
     fontsize_mm = fontsize_pt * pt_to_mm
-    margin_bottom_mm = 10
+    margin_bottom_mm = 20
     character_width_mm = 7 * pt_to_mm
     width_text = a4_width_mm / character_width_mm
 
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+    pdf.set_margins(20, 20, 20)
     pdf.add_page()
-    pdf.set_font(family='Courier', size=fontsize_pt)
+    pdf.set_font(family='Arial', size=fontsize_pt)
     splitted = text.split('\n')
 
     for line in splitted:
-        lines = textwrap.wrap(line, width_text)
+        lines = textwrap.wrap(line, 85)
 
         if len(lines) == 0:
             pdf.ln()
