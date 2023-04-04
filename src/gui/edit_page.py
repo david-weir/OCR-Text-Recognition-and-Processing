bfbfd9f4fa3e05f4b3f4fecb17e4d9218534226c
Róisín O'Rourke <roisin.orourke28@mail.dcu.ie>
2023-04-04 22:52:06 +0100

    connect translation to ui

--- src/gui/edit_page.py
+++ src/gui/edit_page.py
@@ -66,6 +66,11 @@ class EditPage(Frame):
             "fr": "French",
             "de": "German"
         }
+        lang_codes2 = {
+            "English": "en",
+            "French": "fr",
+            "German": "de"
+        }
         select_message = Label(top_frame, text="Please Select:   ")
         detected = detection_text(text_model.get_text())
         
@@ -75,14 +80,14 @@ class EditPage(Frame):
             select_message.pack(side='left', padx=5)
             dropdown.pack(side='left', padx=5)
 
-            Button(top_frame, text="Confirm", command=lambda: text_model.set_src_language(clicked)).pack(side='right', padx=5)
+            Button(top_frame, text="Confirm", command=lambda: text_model.set_src_language(lang_codes2[clicked])).pack(side='right', padx=5)
         else:
             detected_label = Label(top_frame, text="Language Detected: {}".format(lang_codes[detected]))
             detected_label.pack(side='left', padx=5)
 
-            confirm_update = Button(top_frame, text="Confirm", command=lambda: text_model.set_src_language(clicked))
+            confirm_update = Button(top_frame, text="Confirm", command=lambda: text_model.set_src_language(lang_codes2[clicked]))
 
-            confirm = Button(top_frame, text="Confirm", command=lambda: text_model.set_src_language(lang_codes[detected]))
+            confirm = Button(top_frame, text="Confirm", command=lambda: text_model.set_src_language(detected))
 
             change = Button(top_frame, text="Change", command=lambda: 
                      {detected_label.pack_forget(), change.pack_forget(), confirm.pack_forget(), select_message.pack(side='left', padx=5), dropdown.pack(side='left', padx=5), confirm_update.pack(side='right', padx=5)})
--- src/gui/translate_page.py
+++ src/gui/translate_page.py
@@ -8,6 +8,7 @@ from textModel import *
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import translation as t
 
 class TranslatePage(Frame):
     def __init__(self, parent, controller):
@@ -57,7 +58,7 @@ class TranslatePage(Frame):
         select_message = Label(center, text="To").place(relx=0.5, rely=0.5, anchor=CENTER)
         dropdown.place(relx=0.7, rely=0.5, anchor=CENTER)
 
-        confirm = Button(center, text="Confirm", command=lambda:text_model.set_dst_language(lang_codes[clicked])).place(relx=0.5, rely=0.6, anchor=CENTER)
+        confirm = Button(center, text="Confirm", command=lambda: self.confirm(clicked.get())).place(relx=0.5, rely=0.6, anchor=CENTER)
 
         previous = Button(btm_frame, text ="Back",
                    command = lambda : controller.show_frame(edit_page.EditPage))
@@ -66,3 +67,16 @@ class TranslatePage(Frame):
         next = Button(btm_frame, text ="Next",
                command = lambda : controller.show_frame(options_page.OptionsPage))
         next.pack(side='right', padx=8, pady=5)
+
+    def confirm(self, clicked):
+        lang_codes = {
+            "English": "en",
+            "French": "fr",
+            "German": "de"
+        }
+
+        text_model.set_dst_language(lang_codes[clicked])
+        file = text_model.get_textfile()
+        with open(file, 'r') as f:
+            with open("translated.txt", 'w') as ft:
+                ft.write(t.translate(text_model.get_src_language(), text_model.get_dst_language(), f.read()))
