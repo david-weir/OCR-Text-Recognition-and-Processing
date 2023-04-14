d0a7f5b50f16ef5dc3d66cba331ab5ef57472367
Róisín O'Rourke <roisin.orourke28@mail.dcu.ie>
2023-04-14 16:57:16 +0100

    adding summarisation to translate page

--- src/gui/translate_page.py
+++ src/gui/translate_page.py
@@ -5,10 +5,13 @@ import tkinter as tk
 import options_page
 import edit_page
 from textModel import *
+import translation_popup
+import text_preview
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import translation as t
+import summarise
 
 class TranslatePage(Frame):
     def __init__(self, parent, controller):
@@ -28,39 +31,13 @@ class TranslatePage(Frame):
         center.grid(row=1, sticky="nsew")
         btm_frame.grid(row=3, sticky="ew")
 
-        """options = [
-            "Language",
-            "English",
-            "French",
-            "German"
-        ]
+        translate_opt = IntVar()
+        summarise_opt = IntVar()
 
-        lang_codes = {
-            "English": "en",
-            "French": "fr",
-            "German": "de"
-        }
+        Checkbutton(center, text="Translate", variable=translate_opt, onvalue=1, offvalue=0).place(relx=0.5, rely=0.3, anchor=CENTER)
+        Checkbutton(center, text="Summarise", variable=summarise_opt, onvalue=1, offvalue=0).place(relx=0.5, rely=0.4, anchor=CENTER)
 
-        detected = text_model.get_src_language()
-        options.remove(detected)
-        
-        # datatype of menu text
-        clicked = StringVar()
-        
-        # initial menu text
-        clicked.set("Language")
-        
-        # Create Dropdown menu
-        dropdown = OptionMenu(center, clicked, *options)
-        dropdown.config(width=7)
-
-        message = Label(center, text=detected).place(relx=0.3, rely=0.5, anchor=CENTER)
-        select_message = Label(center, text="To").place(relx=0.5, rely=0.5, anchor=CENTER)
-        dropdown.place(relx=0.7, rely=0.5, anchor=CENTER)
-
-        confirm = Button(center, text="Confirm", command=lambda: self.confirm(clicked.get())).place(relx=0.5, rely=0.6, anchor=CENTER)"""
-        b = Button(center, text="click to translate", command=lambda: {self.show_page(center), b.destroy()})
-        b.pack()
+        Button(center, text="Confirm", command= lambda: self.text_options(translate_opt, summarise_opt)).place(relx=0.5, rely=0.6, anchor=CENTER)
 
         previous = Button(btm_frame, text ="Back",
                    command = lambda : controller.show_frame(edit_page.EditPage))
@@ -70,10 +47,29 @@ class TranslatePage(Frame):
                command = lambda : controller.show_frame(options_page.OptionsPage))
         next.pack(side='right', padx=8, pady=5)
 
-    def update_lang(self, center, options, message):
-        detected = text_model.get_src_language()
-        options.remove(detected)
-        message.config(text=detected)
+    def text_options(self, translate_opt, summarise_opt):
+        if translate_opt.get() == 1 and summarise_opt.get() == 1:
+            translation_popup.select_translation()
+            file = text_model.get_textfile()
+            
+            with open("translated.txt", 'w') as ft:
+                summarised = summarise.summarisation(file)
+                ft.write(t.translate(text_model.get_src_language(), text_model.get_dst_language(), summarised))
+            text_preview.view_new_version()
+
+        elif translate_opt.get() == 1:
+            translation_popup.select_translation()
+            
+            file = text_model.get_textfile()
+            with open(file, 'r') as f:
+                with open("translated.txt", 'w') as ft:
+                    ft.write(t.translate(text_model.get_src_language(), text_model.get_dst_language(), f.read()))
+
+        elif summarise_opt.get() == 1:
+            file = text_model.get_textfile()
+            with open("translated.txt", 'w') as ft:
+                ft.write(summarise.summarisation(file))
+            text_preview.view_new_version()
 
     def show_page(self, center):
         options = [
@@ -108,11 +104,11 @@ class TranslatePage(Frame):
         dropdown = OptionMenu(center, clicked, *options)
         dropdown.config(width=7)
 
-        message = Label(center, text=detected).place(relx=0.3, rely=0.5, anchor=CENTER)
-        select_message = Label(center, text="To").place(relx=0.5, rely=0.5, anchor=CENTER)
-        dropdown.place(relx=0.7, rely=0.5, anchor=CENTER)
+        message = Label(center, text=detected).place(relx=0.3, rely=0.7, anchor=CENTER)
+        select_message = Label(center, text="To").place(relx=0.5, rely=0.7, anchor=CENTER)
+        dropdown.place(relx=0.7, rely=0.7, anchor=CENTER)
 
-        confirm = Button(center, text="Confirm", command=lambda: self.confirm(clicked.get())).place(relx=0.5, rely=0.6, anchor=CENTER)
+        confirm = Button(center, text="Confirm", command=lambda: self.confirm(clicked.get())).place(relx=0.5, rely=0.8, anchor=CENTER)
 
     def confirm(self, clicked):
         lang_codes = {
