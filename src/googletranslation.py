244d316e0028fab4d109c353933f6941e3c57286
Róisín O'Rourke <roisin.orourke28@mail.dcu.ie>
2023-04-17 23:11:59 +0100

    add unit tests for detect/translate

--- src/googletranslation.py
+++ src/googletranslation.py
@@ -4,7 +4,9 @@ from pprint import pprint
 # init the Google API translator
 def translate(src, dst, text):
     translator = Translator()
-
+    supported = ['en', 'fr', 'de']
+    if src not in supported:
+        raise ValueError('Language is not supported.')
     # translate a spanish text to english text (by default)
     translation = translator.translate(text)
     # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
--- test/test_translate_detection.py
+++ test/test_translate_detection.py
@@ -25,11 +25,44 @@ class TestDetectionTranslationIntegration(unittest.TestCase):
         with open(fr_filepath, "r") as language_file:
             translated = translate(detected_lang, "en", language_file.read())
         new_detected_lang = detection_text(translated)
+        
         self.assertEqual(detected_lang, "fr")
         self.assertEqual(new_detected_lang, "en")
         with open(en_filepath, "r") as en_file:
             self.assertGreaterEqual(self.normalise_levenshtein(translated, en_file.read()), 0.85)
 
+    def test_unsupported_lang(self):
+        italian_file = 'italiantext.txt'
+        it_filepath = os.path.dirname(__file__) + "/" + italian_file
+
+        detected_lang = detection_file(it_filepath)
+        with open(it_filepath, "r") as language_file:
+            self.assertRaises(ValueError, translate, detected_lang, 'en', language_file.read())
+            
+            with self.assertRaises(ValueError) as cm:
+                translate(detected_lang, 'en', language_file.read())
+                self.assertEqual(str(cm.exception), 'Language is not supported.')
+        
+        self.assertEqual(detected_lang, "it")
+
+    def test_detect_translate_time(self):
+        start_time = time.time()
+
+        german_file = "germantext.txt"
+        de_filepath = os.path.dirname(__file__) + "/" + german_file
+
+        detected_lang = detection_file(de_filepath)
+        with open(de_filepath, "r") as language_file:
+            translated = translate(detected_lang, "en", language_file.read())
+        
+        end_time = time.time()
+        
+        new_detected_lang = detection_text(translated)
+
+        self.assertEqual(detected_lang, "de")
+        self.assertEqual(new_detected_lang, "en")
+        elapsed_time = end_time - start_time
+        self.assertLessEqual(elapsed_time, 10.0)
 
 if __name__ == '__main__':
     unittest.main()
