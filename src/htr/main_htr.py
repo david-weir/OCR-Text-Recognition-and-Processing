0f2b8af91e79914761169a19597f07340c5e80e7
David Weir <david.weir4@mail.dcu.ie>
2023-04-20 00:41:04 +0100

    Implemented main function to handle running htr and an argument parser for user commands on the command line

--- src/htr/main_htr.py
+++ src/htr/main_htr.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from typing import List, Tuple
 
@@ -8,6 +9,7 @@ from htr_model import Model, DecoderType
 from iam_preprocessing import Preprocessor
 from path import Path
 import cv2
+import argparse
 
 class Paths:
     """ Filepaths to data """
@@ -96,6 +98,7 @@ def train(model: Model, loader: IAMLoader, line_mode: bool, early_stopping: int 
             print(f'Limit Reached: No improvement since {early_stopping} epochs. Training stopping...')
             break
 
+
 def validate(model: Model, loader: IAMLoader, line_mode: bool) -> Tuple[float, float]:
     """ Validate NN """
     print('Validate NN')
@@ -139,6 +142,7 @@ def validate(model: Model, loader: IAMLoader, line_mode: bool) -> Tuple[float, f
 
         return char_err_rate, word_accr
 
+
 def infer(model: Model, input_img: Path) -> None:
     """ Recognise text from an individual input image """
     img = cv2.imread(input_img, cv2.IMREAD_GRAYSCALE)
@@ -153,3 +157,63 @@ def infer(model: Model, input_img: Path) -> None:
     print(f'Recognised: "{recognised[0]}"')
     print(f'Probability: {prob[0]}')
 
+
+def parser() -> argparse.Namespace:
+    """ Parse arguments from command line """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mode', help='Choose mode.', choices=['train', 'validate', 'infer'], default='infer')
+    parser.add_argument('--decoder', help='Choose a CTC decoder.', choices=['bestpath', 'beamsearch', 'wordbeamsearch'],
+                        default='bestpath')
+    parser.add_argument('--batch_size', help='Batch size.', type=int, default=100)
+    parser.add_argument('--data_dir', help='Directory containing IAM dataset.', type=Path, required=False)
+    parser.add_argument('--line_mode', help='Train to read text lines instead of single words.', action='store_true')
+    parser.add_argument('--img_file', help='Input image used for inference.', type=Path, default='../data/word.png')
+    parser.add_argument('--early_stopping', help='Early stopping epochs.', type=int, default=25)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parser()  # parse cmd arguments
+    decoder_map = {
+        'bestpath': DecoderType.BestPath,
+        'beamsearch': DecoderType.BeamSearch,
+        'wordbeamsearch': DecoderType.WordBeamSearch
+    }
+    decoderType = decoder_map[args.decoder]  # set CTC decoder
+
+    # if training mode
+    if args.mode == 'train':
+        loader = IAMLoader(args.data_dir, args.batch_size)
+
+        char_list = loader.char_list  # load character list
+
+        # ensure whitespace is in char lst if in line mode
+        if args.line_mode and ' ' not in char_list:
+            char_list = [' '] + char_list
+
+        # save characters and words to corpus text file
+        with open(Paths.corpus, 'w') as f:
+            f.write(''.join(char_list))
+
+        with open(Paths.corpus, 'w') as f:
+            f.write(' '.join(loader.train_words + loader.validation_words))
+
+        model = Model(char_list, decoderType)
+        train(model, loader, line_mode=args.line_mode, early_stopping=args.early_stopping)
+
+    # evaluate model on validation set
+    elif args.mode == 'validate':
+        loader = IAMLoader(args.data_dir, args.batch_size)
+        model = Model(char_lst(), decoderType, restore=True)
+        validate(model, loader, args.line_mode)
+
+    # run inference on inputted image
+    elif args.mode == 'infer':
+        model = Model(char_lst(), decoderType, restore=True)
+        infer(model, args.img_file)
+
+
+if __name__ == '__main__':
+    main()
