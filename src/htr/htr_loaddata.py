b6fb92e4fcf5f7e1d0ec4795bd8ad95d6c4e0200
David Weir <david.weir4@mail.dcu.ie>
2023-04-19 22:12:03 +0100

    Bug fix: broken indexing on validation_samples causing AttributeError: no gt_text for str object

--- src/htr/htr_loaddata.py
+++ src/htr/htr_loaddata.py
@@ -6,8 +6,8 @@ from typing import Tuple
 import numpy as np
 import cv2
 
-Sample = namedtuple('Sample', ('gt_text', 'file_path'))
-Batch = namedtuple('Batch', ('imgs', 'gt_texts', 'batchsize'))
+Sample = namedtuple('Sample', 'gt_text, file_path')
+Batch = namedtuple('Batch', 'imgs, gt_texts, batchsize')
 
 ''' 
 Load data from the IAM dataset  
@@ -21,7 +21,7 @@ class IAMLoader:
         assert data_dir.exists()
 
         self.data_augment = False
-        self.curr_id = 0
+        self.curr_index = 0
         self.batchsize = batchsize
         self.samples = []
 
@@ -55,16 +55,16 @@ class IAMLoader:
                 continue
 
             gt_text = ' '.join(split_line[8:])  # ground text of an image start at col 9 (can continue if considering
-                                                # line/senetnce date)
+            # line/senetnce date)
             chars = chars.union(set(list(gt_text)))
 
             # append samples to Samples list
             self.samples.append(Sample(gt_text, filename))
 
         # divide data into training (95%) and validation (5%) sets
-        split_id = int(data_split * len(self.samples))
-        self.train_samples = self.samples[:split_id]
-        self.validation_samples = self.samples[split_id]
+        split_index = int(data_split * len(self.samples))
+        self.train_samples = self.samples[:split_index]
+        self.validation_samples = self.samples[split_index:]
 
         # populate lists with words
         self.train_words = [x.gt_text for x in self.train_samples]
@@ -79,7 +79,7 @@ class IAMLoader:
     ''' Choose a random subset of the training set '''
     def train_set(self) -> None:
         self.data_augment = True
-        self.curr_id = 0
+        self.curr_index = 0
 
         random.shuffle(self.train_samples)
 
@@ -89,7 +89,7 @@ class IAMLoader:
     ''' Switch from training set to validation set'''
     def validation_set(self) -> None:
         self.data_augment = False
-        self.curr_id = 0
+        self.curr_index = 0
         self.samples = self.validation_samples
         self.curr_set = 'val'
 
@@ -101,15 +101,17 @@ class IAMLoader:
         else:
             num_batches = int(np.ceil(len(self.samples) / self.batchsize))
 
-        curr_batch = self.curr_id // self.batchsize + 1
+        curr_batch = self.curr_index // self.batchsize + 1
         return curr_batch, num_batches
 
     ''' Checks for next element '''
     def has_next(self) -> bool:
         if self.curr_set == 'train':
-            return self.curr_id + self.batchsize <= len(self.samples)  # training set consists of only complete batches
+            # training set consists of only complete batches
+            return self.curr_index + self.batchsize <= len(self.samples)
         else:
-            return self.curr_id < len(self.samples)  # the final batch in the validation set can be smaller (incomplete)
+            # the final batch in the validation set can be smaller (incomplete)
+            return self.curr_index < len(self.samples)
 
     ''' Gets image '''
     def get_img(self, i: int) -> np.ndarray:
@@ -120,11 +122,10 @@ class IAMLoader:
     ''' Get next element '''
     def get_next(self) -> Batch:
         # get range of batches
-        batchrange = range(self.curr_id, min(self.curr_id + self.batchsize, len(self.samples)))
+        batchrange = range(self.curr_index, min(self.curr_index + self.batchsize, len(self.samples)))
 
         imgs = [self.get_img(i) for i in batchrange]
         gt_texts = [self.samples[i].gt_text for i in batchrange]
 
-        self.curr_id += self.batchsize
+        self.curr_index += self.batchsize
         return Batch(imgs, gt_texts, len(imgs))
-
