3a7c2099be955bd98d45828f12ab35bea0121ab3
David Weir <david.weir4@mail.dcu.ie>
2023-04-20 01:44:46 +0100

    Python script to use the LMDB database to speed up image loading

---
+++ src/htr/gen_lmdb.py
@@ -0,0 +1,29 @@
+import argparse
+import pickle
+
+import cv2
+import lmdb
+from path import Path
+
+# handle cmd argument for directory of IAM database
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=Path, required=True)
+args = parser.parse_args()
+
+assert not (args.data_dir / 'lmdb').exists()  # check lmdb does not exist
+
+# map size controls the size of the lmdb database. 2GB covers the IAM dataset (so it can work on 64-bit and 32-bit
+env = lmdb.open(str(args.data_dir / 'lmdb'), map_size=1024 * 1024 * 1024 * 2)
+
+# walk over all png files
+input_imgs = list((args.data_dir / 'img').walkfiles('*.png'))
+
+# convert into lmdb as pickled greyscale images
+with env.begin(write=True) as txn:
+    for i, input_img in enumerate(input_imgs):
+        print(i, len(input_imgs))
+        img = cv2.imread(input_img, cv2.IMREAD_GRAYSCALE)  # read image in OpenCV and convert to greyscale
+        basename = input_img.basename()
+        txn.put(basename.encode("ascii"), pickle.dumps(img))  # represents data as a byte object
+
+env.close()  # close lmdb environ
