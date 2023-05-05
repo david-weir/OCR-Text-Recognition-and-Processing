3264ec0556cce398434e77a0b3024562ddb5983b
Róisín O'Rourke <roisin.orourke28@mail.dcu.ie>
2023-05-05 09:20:02 +0100

    comment detection + add tests

--- test/test_detection.py
+++ test/test_detection.py
@@ -1,30 +1,29 @@
 import unittest
 import os
-from src.detection import detection_text, detection_file
+from src.detection import detection_file
 
 class TestDetection(unittest.TestCase):
 
-    def test_en_detection_text(self):
-        text = "This text is written in English."
-        detected_lang = detection_text(text)
-
-        self.assertEqual(detected_lang, "en")
-
-    def test_en_detection_file(self):
+    # test that the function can detect English
+    def test_en_detection(self):
         file = "englishtext.txt"
         filepath = os.path.dirname(__file__) + "/" + file
         
         detected_lang = detection_file(filepath)
 
         self.assertEqual(detected_lang, "en")
-    
-    def test_fr_detection_text(self):
-        text = "Ce texte est écrit en français."
-        detected_lang = detection_text(text)
 
-        self.assertEqual(detected_lang, "fr")
+    # test that the function can detect English in short file
+    def test_en_detection_short(self):
+        file = "short_eng_text.txt"
+        filepath = os.path.dirname(__file__) + "/" + file
+        
+        detected_lang = detection_file(filepath)
 
-    def test_fr_detection_file(self):
+        self.assertEqual(detected_lang, "en")
+
+    # test that the function can detect French
+    def test_fr_detection(self):
         file = "frenchtext.txt"
         filepath = os.path.dirname(__file__) + "/" + file
 
@@ -32,19 +31,41 @@ class TestDetection(unittest.TestCase):
 
         self.assertEqual(detected_lang, "fr")
 
-    def test_de_detection_text(self):
-        text = "Dieser Text ist in deutscher Sprache verfasst."
-        detected_lang = detection_text(text)
+    # test that the function can detect French in short file
+    def test_fr_detection_short(self):
+        file = "short_fr_text.txt"
+        filepath = os.path.dirname(__file__) + "/" + file
 
-        self.assertEqual(detected_lang, "de")
+        detected_lang = detection_file(filepath)
 
-    def test_de_detection_file(self):
+        self.assertEqual(detected_lang, "fr")
+
+    # test that the function can detect German
+    def test_de_detection(self):
         file = "germantext.txt"
         filepath = os.path.dirname(__file__) + "/" + file
 
         detected_lang = detection_file(filepath)
 
         self.assertEqual(detected_lang, "de")
+
+    # test that the function can detect German in short file
+    def test_de_detection_short(self):
+        file = "short_de_text.txt"
+        filepath = os.path.dirname(__file__) + "/" + file
+
+        detected_lang = detection_file(filepath)
+
+        self.assertEqual(detected_lang, "de")
+
+    # test that the function will return False if it cannot detect a language
+    def test_detection_empty(self):
+        file = "empty.txt"
+        filepath = os.path.dirname(__file__) + "/" + file
+
+        detected_lang = detection_file(filepath)
+
+        self.assertFalse(detected_lang)
 
 if __name__ == '__main__':
     unittest.main()
