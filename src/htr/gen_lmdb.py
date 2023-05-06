d8aec8d1f93ba185c44e581e6f6a5bfca8ec666d
David Weir <david.weir4@mail.dcu.ie>
2023-05-06 23:52:39 +0100

    Small comments addition

--- src/htr/gen_lmdb.py
+++ src/htr/gen_lmdb.py
@@ -13,12 +13,12 @@ args = parser.parse_args()
 assert not (args.data_dir / 'lmdb').exists()  # check lmdb does not exist
 
 # map size controls the size of the lmdb database. 2GB covers the IAM dataset (so it can work on 64-bit and 32-bit
-env = lmdb.open(str(args.data_dir / 'lmdb'), map_size=1024 * 1024 * 1024 * 2)
+env = lmdb.open(str(args.data_dir / 'lmdb'), map_size=1024 * 1024 * 1024 * 2)  # new lmdb environment
 
 # walk over all png files
 input_imgs = list((args.data_dir / 'img').walkfiles('*.png'))
 
-# convert into lmdb as pickled greyscale images
+# convert into lmdb as pickled greyscale images - new write transaction
 with env.begin(write=True) as txn:
     for i, input_img in enumerate(input_imgs):
         print(i, len(input_imgs))
